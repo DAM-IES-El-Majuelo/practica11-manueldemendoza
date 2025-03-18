2a191974d1abdded67c70381e4a897b0930d3db4
manueldemendoza <154356087+manueldemendoza@users.noreply.github.com>
2025-03-18 12:03:52 +0100

    Create Deportista.java

---
+++ src/practica11/manueldemendoza/Deportista.java
@@ -0,0 +1,30 @@
+package practica11.manueldemendoza;
+
+public class Deportista extends Persona {
+    private float peso;
+    private float estatura;
+
+    // Constructor
+    public Deportista(int id, String nombre, float sueldo, float peso, float estatura) {
+        super(id, nombre, sueldo);
+        this.peso = peso;
+        this.estatura = estatura;
+    }
+
+    // Getters and Setters
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+}
