01e1584af08c2cc47030114a7b0f8732a15d9357
alumno <manueldemendoza@gmail.com>
2025-03-18 11:35:03 +0100

    7 commit

---
+++ src/practica11/manueldemendoza/Persona.java
@@ -0,0 +1,56 @@
+package practica11.manueldemendoza;
+
+/**
+ *
+ * @author alumno
+ */
+public class Persona {
+    
+    private int id;
+    private String nombre;
+    private float sueldo;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the sueldo
+     */
+    public float getSueldo() {
+        return sueldo;
+    }
+
+    /**
+     * @param sueldo the sueldo to set
+     */
+    public void setSueldo(float sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    
+}
