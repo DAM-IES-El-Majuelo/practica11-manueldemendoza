4fda8c931def147a824322f74b75ad2c84b3a512
manueldemendoza <154356087+manueldemendoza@users.noreply.github.com>
2025-03-18 10:46:22 +0100

    Update Practica11Manueldemendoza.java

--- src/practica11/manueldemendoza/Practica11Manueldemendoza.java
+++ src/practica11/manueldemendoza/Practica11Manueldemendoza.java
@@ -16,6 +16,7 @@ public class Practica11Manueldemendoza {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo de nuevo");
+        System.out.println("Hola mundo de nuevo");
     }
     
 }
